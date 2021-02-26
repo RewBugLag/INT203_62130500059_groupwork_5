3bd7845bf6d73f236a65dd61f7d7f8fab55b790d
RewBugLag <rewrew2000@gmail.com>
2021-02-27 01:36:08 +0700

    Added PhotoSearch component

--- 62130500059_groupwork_5/components/PhotoSearch.js
+++ 62130500059_groupwork_5/components/PhotoSearch.js
@@ -1,19 +1,30 @@
 app.component('photo-search', {
     props: {
-
+        search: Boolean
     },
     template: 
+    /*html*/
     `
-
-    `,
-
+    <button class="material-icons" v-if="search==false" v-on:click="toggleSearch">search</button>
+    <div class="flex" v-show="search">
+        <input type="text" class="flex-1 border-2 border-blue-2" 
+            v-model="input" @keyup.enter="searchPicture"
+            placeholder="Please enter text for searching photos">
+        <button class="bg-blue-400 text-white py-1 px-6 rounded" v-on:click="toggleSearch">Cancel</button>
+    </div>
+    `
+    ,
     data() {
         return {
-
+            input: ''
         }
     },
-
     methods: {
-        
+        toggleSearch() {
+            this.$emit('toggle-search')
+        },
+        searchPicture() {
+            this.$emit('search-picture')
+        }
     }
 })
